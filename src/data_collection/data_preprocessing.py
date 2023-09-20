545f00c608a80ac144e25b9575e3526dd8a45dd9
Luke <luke.freund@mail.mcgill.ca>
2023-09-20 15:00:13 -0400

    random forest model implementation

--- src/data_collection/data_preprocessing.py
+++
@@ -1,5 +0,0 @@
-def handle_missing_values(data):
-    data.fillna(0, inplace=True)
-    return data
-
-
--- src/data_collection/data_source.py
+++ src/data_collection/data_source.py
@@ -3,15 +3,32 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 
-def load_data():
-    filepath = '../../data/credit_risk_dataset.csv'
+def load_data(filepath):
     data = pd.read_csv(filepath)
     return data
+
+
+def data_preprocessing(data):
+    # Fill in missing values with mode value.
+    data['person_emp_length'].fillna(data['person_emp_length'].mode()[0], inplace=True)
+    data['loan_int_rate'].fillna(data['loan_int_rate'].mode()[0], inplace=True)
+
+    # replace loan grade with numbers
+    grade_mapping = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
+    data['loan_grade'] = data['loan_grade'].map(grade_mapping)
+
+    # Convert categorical into numerical values
+    return pd.get_dummies(data, columns=['person_home_ownership', 'loan_intent', 'cb_person_default_on_file'],
+                          drop_first=True)
+
+
+from sklearn.preprocessing import StandardScaler
 
 
 def scale_features(data):
     scaler = StandardScaler()
     numerical_cols = data.select_dtypes(include=['float64', 'int64']).columns
+    numerical_cols = numerical_cols.drop('loan_status')  # Exclude 'loan_status' from scaling
     data[numerical_cols] = scaler.fit_transform(data[numerical_cols])
     return data
 
@@ -23,29 +40,18 @@ def split_data(data, target_column):
     return X_train, X_test, y_train, y_test
 
 
-data = load_data()
-
-# Fill in missing values with mode value.
-data['person_emp_length'].fillna(data['person_emp_length'].mode()[0], inplace=True)
-data['loan_int_rate'].fillna(data['loan_int_rate'].mode()[0], inplace=True)
-
-# replace loan grade with numbers
-grade_mapping = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
-data['loan_grade'] = data['loan_grade'].map(grade_mapping)
-
-# Convert categorical into numerical values
-data = pd.get_dummies(data, columns=['person_home_ownership', 'loan_intent', 'cb_person_default_on_file'],
-                      drop_first=True)
-
-# Scale features
-data = scale_features(data)
-
-# Split the data
-X_train, X_test, y_train, y_test = split_data(data, 'loan_status')
-
-print(data.head())
-print(data.describe())
-print(data.info())
+# data = load_data()
+#
+#
+# # Scale features
+# data = scale_features(data)
+#
+# # Split the data
+# X_train, X_test, y_train, y_test = split_data(data, 'loan_status')
+#
+# print(data.head())
+# print(data.describe())
+# print(data.info())
 
 
 
